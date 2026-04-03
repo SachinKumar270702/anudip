a2b321d917796bb926ac1ead45d2cedce472185a
Sachin <sachin@example.com>
2026-04-03 19:20:43 +0530

    push test

---
+++ src/classes/ifElseConditions.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class ifElseConditions {
+    static void main() {
+        String a="hello";
+        String b="Hello";
+        System.out.println(a.equals(b));//case sensitive
+        System.out.println(a.equalsIgnoreCase(b));// only compare value case doesn't matters;
+
+    }
+
+}
